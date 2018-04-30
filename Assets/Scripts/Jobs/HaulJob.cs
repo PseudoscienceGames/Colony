d3c39c6c0f514172da10a0e1f42813a447eccce5
PseudoscienceGames <quade.81@gmail.com>
2018-04-30 18:06:28 -0500

    started working on jobs

---
+++ Assets/Scripts/Jobs/HaulJob.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaulJob : Job
+{
+	public Vector3 targetLocation;
+	public List<Item> items  = new List<Item>();
+}
